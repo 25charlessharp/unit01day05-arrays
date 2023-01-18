455b1451cdd794ba6852c283719053390161bde5
Charles Sharp <25charlessharp@usd266.com>
2023-01-18 12:01:33 -0600

    getting started

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
 public class ImmutableSort {
     public static void main(String[] args) {
         int[] a = {2, 2, 2};
-        System.out.println(a[0]*a[1]*a[2]);
+        int[] sortedArray = new int[] {a[0], a[1], a[2]};
+        int largest = a[0];
+        int between = a[1];
+        int smallest = a[2];
+
+        System.out.println("Original Array: "+ Arrays.toString(a));
+        }
     }
-}
