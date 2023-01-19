d0fa3121e67445d5e60855f87f9dadcbb79b28ad
Charles Sharp <25charlessharp@usd266.com>
2023-01-19 12:09:24 -0600

    Testing it out

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -2,11 +2,36 @@ import java.util.Arrays;
 public class ImmutableSort {
     public static void main(String[] args) {
         int[] a = {2, 2, 2};
-        int[] sortedArray = new int[] {a[0], a[1], a[2]};
-        int largest = a[0];
+
+        int largest = a[2];
         int between = a[1];
-        int smallest = a[2];
+        int smallest = a[0];
+        int place = 0;
+        int place2 = 0;
+
+        if ( smallest > between){
+            place = between;
+            place2 = smallest;
+            smallest = place;
+            between = place2;
+        }
+        if (smallest > largest){
+            place = smallest;
+            place2 = largest;
+            smallest = place2;
+            largest = place;
+        }
+        if (between > largest){
+            place = between;
+            place2 = largest;
+            between = place2;
+            largest = place;
+        }
+        int[] sortedArray = new int[] {smallest + between + largest};
+
 
         System.out.println("Original Array: "+ Arrays.toString(a));
+        System.out.println("New Array: " + Arrays.toString(sortedArray));
+
         }
     }
