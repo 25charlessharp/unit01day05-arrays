4cc7ffd5e118fbd5f1a28dd9089f06c82fc00010
Charles Sharp <25charlessharp@usd266.com>
2023-01-19 12:16:52 -0600

    Paste part from ImmutableSort

--- MutableSort.java
+++ MutableSort.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
 public class MutableSort {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        int largest = a[2];
+        int between = a[1];
+        int smallest = a[0];
+        int place = 0;
+        int place2 = 0;
+
+        if (smallest > between){
+            place = between;
+            place2 = smallest;
+            smallest = place;
+            between = place2;
+        }
+        if (smallest > largest){
+            place = smallest;
+            place2 = largest;
+            smallest = place2;
+            largest = place;
+        }
+        if (between > largest){
+            place = between;
+            place2 = largest;
+            between = place2;
+            largest = place;
+        }
+        int[] sortedArray = new int[] {smallest, between, largest};
+        System.out.println(Arrays.toString(sortedArray));
     }
 }
